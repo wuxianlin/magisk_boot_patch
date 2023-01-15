92d729b659d5f60bf2f695bb182dbbc28da97303
wuxianlin <5378164+wuxianlin@users.noreply.github.com>
2023-01-16 01:49:27 +0800

    Update repack.sh

--- repack.sh
+++ repack.sh
@@ -10,10 +10,10 @@ for image in `find imgs -name recovery.img -o -name boot.img -o -name vendor_boo
     ramdisk=AIK-Linux/ramdisk
     for prop_file in `grep -rEl '^(ro.debuggable|ro.secure|ro.adb.secure|persist.sys.usb.config)=' $ramdisk|grep -E '(prop|default)$'`;do
         echo mod $prop_file
-        sed -i 's/ro.secure=1/ro.secure=0/g' $prop_file
-        sed -i 's/ro.adb.secure=1/ro.adb.secure=0/g' $prop_file
-        sed -i 's/ro.debuggable=0/ro.debuggable=1/g' $prop_file
-        sed -i 's/persist.sys.usb.config=none/persist.sys.usb.config=adb/g' $prop_file
+        sed -i '' 's/ro.secure=1/ro.secure=0/g' $prop_file
+        sed -i '' 's/ro.adb.secure=1/ro.adb.secure=0/g' $prop_file
+        sed -i '' 's/ro.debuggable=0/ro.debuggable=1/g' $prop_file
+        sed -i '' 's/persist.sys.usb.config=none/persist.sys.usb.config=adb/g' $prop_file
     done
     AIK-Linux/repackimg.sh
     bash -x ./resign.sh AIK-Linux/image-new.img $image
